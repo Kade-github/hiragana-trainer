46f8a0ebe019f301140dff2354014ed57f152334
Kade <KadePcGames@gmail.com>
2023-03-01 21:58:05 -0800

    add main files

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require('fs').promises;
+
+const requestListener = function (req, res) {
+    let p = req.url;
+    if (p == undefined || p == "/")
+        p = "/index.html";
+    fs.readFile(__dirname + p)
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(404);
+            res.end("404 lol");
+            return;
+        });
+};
+
+const server = http.createServer(requestListener);
+server.listen(8000, 'localhost', () => {
+    console.log(`Server is running on localhost:8000`);
+    
+});
